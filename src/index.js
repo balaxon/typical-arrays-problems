8ad92518fa2b73d40bdaebb7f90c16c719e812c0
balaxon <balahanov2209@gmail.com>
2020-03-21 23:37:52 +0300

    feat: all done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!arguments.length || array.length==0){
+    return 0;
+  }
+  array.sort(function(a,b){return a-b});
+  return array[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!arguments.length || array==0){
+    return 0;
+  }
+  array.sort(function(a,b){return a-b});
+  return array[array.length-1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!arguments.length || array.length==0){
+    return 0;
+  }
+  let a = 0;
+  for(let i=0;i<array.length; i++){
+    a+= array[i];
+  }
+  return a/array.length;
 }
